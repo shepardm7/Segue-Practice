9880403f1f5761afaeff92b1cd6ca5e2936440f5
Sateek Roy <Phoenix@Sateeks-Mac.local>
2018-03-02 17:15:50 -0500

    First Commit

---
+++ Segue Practice/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Segue Practice
+//
+//  Created by Sateek Roy on 2017-07-22.
+//  Copyright © 2017 SateekLambton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToFirst(_ sender: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
